0de281390f3d82cc49e241c6a6a3250a1b768835
Kielx <kiel66@gmail.com>
2021-08-13 22:06:39 +0200

    Add to be customized navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="font-normal text-3xl text-center">Hello world</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <>
+            <Navbar></Navbar>
+            <h1>Hello from index</h1>
+            <header className="bg-gray-50 w-full h-36"></header>
+          </>
+        </Route>
+        <Route path="*">
+          <h1>Oooops... Requested page was not found</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
