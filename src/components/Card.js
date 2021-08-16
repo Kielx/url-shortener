15884f9d38e0e26e8ea16e7828f8277483fe950d
Kielx <kiel66@gmail.com>
2021-08-16 14:39:03 +0200

    Cards layout done for mobile and desktop

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,20 @@
 import React from "react";
 
-const Card = ({ header, text, icon }) => {
+const Card = ({ header, text, icon, index }) => {
   return (
-    <div className="flex flex-col max-w-sm pb-6 bg-white mx-4 rounded-md px-12 ">
-      <div className="rounded-full bg-darkViolet m-auto p-5 relative -top-10">
+    <div
+      style={{ zIndex: "2" }}
+      className={`top-${
+        index * 10
+      } flex flex-col max-w-sm pb-6 bg-white mx-4 rounded-md px-12 lg:relative`}
+    >
+      <div className="lg:absolute lg:left-8 rounded-full bg-darkViolet m-auto p-5 relative -top-10">
         <img src={icon} alt="icon" />
       </div>
-      <h4 className="relative -top-6 text-xl text-center font-bold">
+      <h4 className="lg:text-left lg:pt-16 lg:top-0 relative -top-6 text-xl text-center font-bold">
         {header}
       </h4>
-      <p className="text-gray-500 text-center">{text}</p>
+      <p className="lg:text-left lg:pt-4 text-gray-500 text-center">{text}</p>
     </div>
   );
 };
--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -7,14 +7,14 @@ import iconFullyCustomizable from "../images/icon-fully-customizable.svg";
 
 const cards = [
   {
-    header: "Detailed Records",
-    text: "Gain insights into who is clicking your links. See who is using your site and how often.",
-    icon: detailedRecordsIcon,
-  },
-  {
     header: "Brand Recognition",
     text: "Brand Recognition Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
     icon: brandRecognitionIcon,
+  },
+  {
+    header: "Detailed Records",
+    text: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps you make more informed decisions.",
+    icon: detailedRecordsIcon,
   },
   {
     header: "Fully Customizable",
@@ -25,22 +25,27 @@ const cards = [
 
 const Statistics = () => {
   return (
-    <div className="bg-gray-100 pb-20">
+    <div className="bg-gray-100 pb-20" style={{ zIndex: "0" }}>
       <Shorten />
       <div className="flex flex-col items-center">
         <h3 className="font-bold text-2xl text-center">Advanced Statistics </h3>
-        <p className="pb-20 pt-6 text-md text-center text-gray-500">
+        <p className="max-w-sm  pb-32 pt-6 text-md text-center text-gray-500">
           Track how your links are performing across the web with our advanced
           statistics dashboard.
         </p>
-        <div className="">
+        <div className="lg:flex lg:flex-row relative">
           {cards.map((card, index) => (
             <>
-              <Card key={card.header} {...card} />
+              <Card key={card.header} {...card} index={index} />
 
               {index < cards.length - 1 ? (
-                <div className="md:hidden bg-cyan w-2 h-24 m-auto"></div>
-              ) : null}
+                <div className="lg:hidden bg-cyan w-2 h-24 m-auto"></div>
+              ) : (
+                <div
+                  style={{ zIndex: "1" }}
+                  className="hidden lg:block bg-cyan w-8/12 h-2 m-auto absolute top-32 left-10"
+                ></div>
+              )}
             </>
           ))}
         </div>
