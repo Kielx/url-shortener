f1a5b778bb9a48f65c578aeedbb109ab56a765a4
Kielx <kiel66@gmail.com>
2021-08-17 20:36:28 +0200

    Add working url shortening!

--- src/components/Shorten.js
+++ src/components/Shorten.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import backgroundMobile from "../images/bg-shorten-mobile.svg";
 import backgroundDesktop from "../images/bg-shorten-desktop.svg";
+import ShortenLink from "./ShortenLink";
+import LoadingSpinner from "./LoadingSpinner";
 
 const checkScreenWidth = () => {
   if (typeof window !== `undefined`) {
@@ -10,6 +11,22 @@ const checkScreenWidth = () => {
 };
 
 const Shorten = () => {
+  const [longLink, setLongLink] = useState("");
+  const [linkList, setLinkList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function shortenLink(link) {
+    setLoading(true);
+    const response = await fetch(
+      `https://api.shrtco.de/v2/shorten?url=${link}`
+    );
+    let data = await response.json();
+    setLoading(false);
+    if (data.ok) {
+      return setLinkList((oldList) => [...oldList, data.result]);
+    }
+  }
+
   return (
     <>
       <section
@@ -25,20 +42,34 @@ const Shorten = () => {
           type="text"
           className="md:w-10/12 whitespace-nowrap inline-flex items-center justify-center px-4 py-3 border border-transparent rounded-md shadow-sm text-base font-medium placeholder-gray-400 "
           placeholder="Shorten a link here..."
+          onChange={(e) => setLongLink(e.target.value)}
         ></input>
-        <button className="md:w-2/12 whitespace-nowrap inline-flex items-center justify-center px-4 py-3 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-cyan hover:bg-teal-500">
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            shortenLink(longLink);
+          }}
+          className="md:w-2/12 whitespace-nowrap inline-flex items-center justify-center px-4 py-3 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-cyan hover:bg-teal-500"
+        >
           Shorten it!
         </button>
       </section>
-      <section className="relative md:divide-y-0 divide-y -top-20 mt-4 md:px-10 md:py-3 md:space-x-4 md:space-y-0 md:items-center flex flex-col md:flex-row w-10/12 m-auto p-5 rounded-lg bg-white bg-cover bg-no-repeat space-y-4">
-        <div className="md:w-7/12">https://Longlinkdotorg.com/12&321#22</div>
-        <Link to="#" className="md:text-right md:w-3/12 text-cyan pt-4 md:pt-0">
-          https://Short.cc/12
-        </Link>
-        <button className="md:w-2/12 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-bold text-white bg-cyan hover:bg-teal-500">
-          Copy
-        </button>
-      </section>
+      {linkList.map((link, index) => (
+        <ShortenLink
+          key={index}
+          longLink={link.original_link}
+          shortLink={link.short_link}
+          fullShortLink={link.full_short_link}
+        />
+      ))}
+      {loading ? (
+        <section className="relative md:divide-y-0 divide-y -top-20 mt-4 md:px-10 md:py-3 md:space-x-4 md:space-y-0 md:items-center flex flex-col md:flex-row w-10/12 m-auto p-5 rounded-lg bg-white bg-cover bg-no-repeat space-y-4">
+          <span className="text-gray-400">
+            {`Shortening your URL. Please wait, it might take a (long) while...`}
+          </span>
+          <LoadingSpinner />
+        </section>
+      ) : null}
     </>
   );
 };
